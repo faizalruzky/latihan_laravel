a105f38c75f2033c360ee27ca4fea0a9331d555e
faizalruzky <faizalruzky@gmail.com>
2016-08-19 16:37:59 +0700

    upload image

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,6 +1,7 @@
 @extends("layouts.application")
 @section("content")
   <div>
+    <img src="{{ asset('/uploads/images/'. $article->id . '/' . $article->image) }}">
     <h3>{!! $article->title !!}</h3>
     <p>{!! $article->content!!}</p>
     <i>By {!! $article->author !!}</i>

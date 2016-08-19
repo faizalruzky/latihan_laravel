eb066f14406290a6bc76e11fd70033fb430ae976
faizalruzky <faizalruzky@gmail.com>
2016-08-19 16:28:13 +0700

    tambah field image

--- app/Article.php
+++ app/Article.php
@@ -7,13 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = [
-          'title', 'content', 'author',
+          'title', 'content', 'author', 'foto', 
         ]; //
          public static function valid($id='') {
       	 return array(
         'title' => 'required|min:10|unique:articles,title'.($id ? ",$id" : ''),
         'content' => 'required|min:100|unique:articles,content'.($id ? ",$id" : ''),
-        'author' => 'required'
+        'author' => 'required',
+        'foto' => 'mimes:jpeg,bmp,png'
       );
     }
 }

586da0e0a58eb1d78f817d354fc315e4bb83805d
faizalruzky <faizalruzky@gmail.com>
2016-08-21 23:15:59 +0700

    tampil foto yang sudah di resize

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.application")
 @section("content")
   <div>
-    <img src="{{ asset('/uploads/images/'. $article->id . '/' . $article->image) }}">
+    <img src="{{ asset('/uploads/images/'. $article->id . '/resize-' . $article->image) }}">
     <h3>{!! $article->title !!}</h3>
     <p>{!! $article->content!!}</p>
     <i>By {!! $article->author !!}</i>

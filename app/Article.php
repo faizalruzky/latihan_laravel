4ea3ad7eb0f5f19b88a194748cb97b88c5e5255f
faizalruzky <faizalruzky@gmail.com>
2016-08-21 23:08:20 +0700

    tambah validasi image

--- app/Article.php
+++ app/Article.php
@@ -11,10 +11,10 @@ class Article extends Model
         ]; //
          public static function valid($id='') {
       	 return array(
-        'title' => 'required|min:10|unique:articles,title'.($id ? ",$id" : ''),
-        'content' => 'required|min:100|unique:articles,content'.($id ? ",$id" : ''),
+        'title' => 'required|min:5|unique:articles,title'.($id ? ",$id" : ''),
+        'content' => 'required|min:10|unique:articles,content'.($id ? ",$id" : ''),
         'author' => 'required',
-        'foto' => 'mimes:jpeg,bmp,png'
+        'foto' => 'mimes:jpeg,bmp,png|max:500'
       );
     }
 }

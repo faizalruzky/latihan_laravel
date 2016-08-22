353a1e1746d68d16d9db0ada26052a7014710fd1
faizalruzky <faizalruzky@gmail.com>
2016-08-22 16:29:13 +0700

    index dengan tampilan tabel

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,47 +1,26 @@
 @extends("layouts.application")
-<!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="css/style.css" rel="stylesheet">
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-      <!--Import bootstrap.css-->
-      <link href="css/bootstrap.min.css" rel="stylesheet"/>
-      <!--Import Google Icon Font-->
-      <!--Import materialize.css-->
-      <link href="css/materialize.min.css" rel="stylesheet"/>
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <script src="js/jquery-3.1.0.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <script src="js/materialize.min.js"></script>
-    </head>
-
 @section("content")
- <body>
-  <div>{!! link_to('articles/create', 'Write Article', array('class' => 'btn btn-success')) !!}</div>
-  @foreach ($articles as $article)
-
+    <h3>Article</h3>
+    <a href="{{ url('/articles/create') }}" class="btn btn-primary btn-xs" title="Add New Article"><span class="glyphicon glyphicon-plus" aria-hidden="true"/></a>
+    </div>
+  <!-- <div>{!! link_to('articles/create', 'Write Article', array('class' => 'btn btn-success')) !!}</div> -->
+  <div style="padding-top:5px">
+  <div class="table">
     <table class="table table-bordered table-striped table-hover">
-    <thead>
+      <thead>
                 <tr>
-                    <th>No</th><th> Title </th><th> Content </th><th>Images</th><th>Author</th><th>Actions</th>
+                    <th>No</th><th> Title</th><th>Content</th><th>Images</th><th>Author</th><th>Actions</th>
                 </tr>
-            </thead>
-      <tbody>
-       {{-- */$x=0;/* --}}
+      </thead>
+       <tbody>
+            {{-- */$x=0;/* --}}
             @foreach($articles as $article)
                 {{-- */$x++;/* --}}
-        <tr>
-         <td>{{ $x }}</td>
-          <td data-field="id">{{$article->title}}</td>
-          <td data-field="id">{{$article->content}}</td>
-          <td data-field="id">{{$article->image}}</td>
-          <td data-field="id">{{$article->author}}</td>
-          <td>
-       
+                <tr>
+                    <td>{{ $x }}</td>
+                    <td>{{ $article->title }}</td><td>{{ $article->content }}</td><td>{{ $article->image }}</td><td>{{ $article->author }}</td>
+                    <td>          
+    <div>
       <div>
       {!! link_to('articles/'.$article->id, 'Show', array('class' => 'btn btn-info')) !!}
       {!! link_to('articles/'.$article->id.'/edit', 'Edit', array('class' => 'btn btn-warning')) !!}
@@ -49,27 +28,11 @@
         {!! Form::submit('Delete', array('class' => 'btn btn-danger', "onclick" => "return confirm('are you sure?')")) !!}
       {!! Form::close() !!}
       </div>
-          </td>
-        </tr>
-         @endforeach
-      </tbody>
+    </div>
+    </tr>
+     @endforeach
+    </tbody>
     </table>
-
-   <!--  <div>
-      <h3>{{$article->title}}</h3>
-      <p>{{$article->content}}</p>
-      <i>By {{$article->author}}</i>
-      <div>
-      {!! link_to('articles/'.$article->id, 'Show', array('class' => 'btn btn-info')) !!}
-      {!! link_to('articles/'.$article->id.'/edit', 'Edit', array('class' => 'btn btn-warning')) !!}
-      {!! Form::open(array('route' => array('articles.destroy', $article->id), 'method' => 'delete')) !!}
-        {!! Form::submit('Delete', array('class' => 'btn btn-danger', "onclick" => "return confirm('are you sure?')")) !!}
-      {!! Form::close() !!}
-      </div>
-    </div> -->
-<!--   @endforeach -->
-   
-    </body>
-  </html>
-
+    </div>
+ 
 @stop

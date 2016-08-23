777ddc2f17d675518e38694ecdf6e23584aaf965
faizalruzky <faizalruzky@gmail.com>
2016-08-23 20:18:35 +0700

    tambah css

---
+++ public/library/masonry/jquery.js
@@ -0,0 +1,16 @@
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});

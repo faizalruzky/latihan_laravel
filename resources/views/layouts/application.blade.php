ad748d1ddf041d11e845f0378a90bc9d7cd6c054
faizalruzky <faizalruzky@gmail.com>
2016-08-23 19:08:46 +0700

    tambah css

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -5,38 +5,53 @@
       <meta httpequiv="XUACompatible" content="IE=edge">
       <meta name="viewport" content="width=devicewidth, initialscale=1">
       <title>FaizalRuzky</title>
-      <link href="/css/bootstrap.min.css" rel="stylesheet" />
-      <link href="/css/materialize.min.css" rel="stylesheet" />
+      <!--Import library css-->
+      <link href="/library/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+      <link href="/library/materialize/css/materialize.min.css" rel="stylesheet"/>
+     <!--  <link href="/library/masonry/style.css" rel="stylesheet">
+      <link href="/library/masonry/normalize.css" rel="stylesheet"> -->
+      
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/custom/style.css">
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700">
+      
+      <script src="/library/jquery/jquery-3.1.0.js"></script>
+      <script src="/library/bootstrap/js/bootstrap.min.js"></script>
+      <script src="/library/materialize/js/materialize.min.js"></script>
+      <script src="/library/masonry/masonry.pkgd.min.js"></script>
+      <script src="http://code.jquery.com/jquery.js"></script>
+ 
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js"></script>
+      <script src="/custom/script.js"></script>
     </head>
-    <body style="padding-top:60px;">
+    
+    <body>
       <!--bagian navigation-->
       @include('shared.head_nav')
       <!-- Bagian Content -->
       <div class="container clearfix">
-       <!--  <div class="row row-offcanvas row-offcanvas-left "> -->
-          <!--Bagian Kiri-->
-          <!-- @include("shared.left_nav") -->
-
-          <!--Bagian Kanan-->
-          <div id="main-content">
-            <div class="panel-body">
-              @if (Session::has('error'))
-                <div class="session-flash alert-danger">
-                    {{Session::get('error')}}
-                </div>
-              @endif
-              @if (Session::has('notice'))
-                <div class="session-flash alert-info">
-                    {{Session::get('notice')}}
-                </div>
-              @endif
-              @yield("content")
-            </div>
-          </div>
+        <!--  <div class="row row-offcanvas row-offcanvas-left "> -->
+        <!--Bagian Kiri-->
+        <!-- @include("shared.left_nav") -->
+        <!--Bagian Kanan-->
+        <div id="main-content">
+          <div class="panel-body">
+            @if (Session::has('error'))
+              <div class="session-flash alert-danger">
+                  {{Session::get('error')}}
+              </div>
+            @endif
+            @if (Session::has('notice'))
+              <div class="session-flash alert-info">
+                  {{Session::get('notice')}}
+              </div>
+            @endif
+            @yield("content")
+          <!-- </div> -->
         </div>
       </div>
-      <script src="/js/jquery-3.1.0.js"></script>
-      <script src="/js/bootstrap.js"></script>
-      <script src="/js/materialize.js"></script>
+      <link href="/library/masonry/style.css" rel="stylesheet"/>
+      <script src="/library/masonry/jquery.js" type="text/javascript"></script>
     </body>
   </html>

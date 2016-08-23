4d4635be826a1e127914d7b02bb3b42aa902a8c4
faizalruzky <faizalruzky@gmail.com>
2016-08-23 19:05:45 +0700

    tampilan masonry

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,38 +1,31 @@
 @extends("layouts.application")
 @section("content")
-    <h3>Article</h3>
-    <a href="{{ url('/articles/create') }}" class="btn btn-primary btn-xs" title="Add New Article"><span class="glyphicon glyphicon-plus" aria-hidden="true"/></a>
+  <header id="header" class="container">
+    <h1>Articles</h1>
+    <a href="{{ url('/articles/create') }}"><button  class="btn btn-primary btn-xs" title="Add New Article"><span class="glyphicon glyphicon-plus" aria-hidden="true"/></button></a>
+    </header>
+  
+  
+  
+  <!-- Posts -->
+  @foreach($articles as $article)
+     <div class="row">
+        <div class="col s4 m4">
+          <div class="card small">
+            <div class="card-image">
+               <img src="{{ asset('/uploads/images/'. $article->id . '/' . $article->image) }} " class="img-responsive">
+              		<span class="card-title">{{($article->title)}} </span>
+            </div>
+            <div class="card-content">
+              <p>{{($article->content)}}</p>
+            </div>
+            <div class="card-action">
+              <a href="{{url('articles/'.$article->id)}}">Read more</a>
+               <a href="{{ url('articles/'.$article->id. '/edit') }}"><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></button></a> 
+                <a href="{{ url('articles/'.$article->id. '/delete') }}" onclick="return confirm('Are you sure?')"><button type="button" class="btn btn-danger"> <span class="glyphicon glyphicon-trash"> </span></button></a>  
+            </div>
+          </div>
+        </div>
+        @endforeach
     </div>
-  <!-- <div>{!! link_to('articles/create', 'Write Article', array('class' => 'btn btn-success')) !!}</div> -->
-  <div style="padding-top:5px">
-  <div class="table">
-    <table class="table table-bordered table-striped table-hover">
-      <thead>
-                <tr>
-                    <th>No</th><th> Title</th><th>Content</th><th>Images</th><th>Author</th><th>Actions</th>
-                </tr>
-      </thead>
-       <tbody>
-            {{-- */$x=0;/* --}}
-            @foreach($articles as $article)
-                {{-- */$x++;/* --}}
-                <tr>
-                    <td>{{ $x }}</td>
-                    <td>{{ $article->title }}</td><td>{{ $article->content }}</td><td>{{ $article->image }}</td><td>{{ $article->author }}</td>
-                    <td>          
-    <div>
-      <div>
-      {!! link_to('articles/'.$article->id, 'Show', array('class' => 'btn btn-info')) !!}
-      {!! link_to('articles/'.$article->id.'/edit', 'Edit', array('class' => 'btn btn-warning')) !!}
-      {!! Form::open(array('route' => array('articles.destroy', $article->id), 'method' => 'delete')) !!}
-        {!! Form::submit('Delete', array('class' => 'btn btn-danger', "onclick" => "return confirm('are you sure?')")) !!}
-      {!! Form::close() !!}
-      </div>
-    </div>
-    </tr>
-     @endforeach
-    </tbody>
-    </table>
-    </div>
- 
 @stop

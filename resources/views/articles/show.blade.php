6592823475cbac1efde8d517b9605ecac7a84698
faizalruzky <faizalruzky@gmail.com>
2016-08-23 22:51:56 +0700

    klik gambar

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,10 +1,14 @@
 @extends("layouts.application")
 @section("content")
   <div>
-    <img src="{{ asset('/uploads/images/'. $article->id . '/resize-' . $article->image) }}">
+    <img class="materialboxed" src="{{ asset('/uploads/images/'. $article->id . '/resize-' . $article->image) }}">
     <h3>{!! $article->title !!}</h3>
     <p>{!! $article->content!!}</p>
     <i>By {!! $article->author !!}</i>
   </div>
-
+<script type="text/javascript">
+	$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+</script>
 @endsection

3b5a75033ccdbb2b8b3adeaff6d93ffd2b2ca9ad
faizalruzky <faizalruzky@gmail.com>
2016-08-24 15:43:38 +0700

    tambah relasi dengan comments

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,6 +8,7 @@ use Validator;
 use Session;
 use App\Http\Requests;
 use App\Article;
+use App\Comment;
 use Image,File;
 class ArticlesController extends Controller
 {
@@ -90,8 +91,8 @@ class ArticlesController extends Controller
     public function show($id)
     {
         $article = Article::find($id);
-        return view('articles.show')
-        ->with('article', $article);
+        $comments = Article::find($id)->comments->sortBy('Comment.created_at');
+        return view('articles.show')->with('article', $article)->with('comments', $comments);
         //
     }
 

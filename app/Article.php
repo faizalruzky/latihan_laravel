444bddea70a319a8fa810cce7de8c9bc20423666
faizalruzky <faizalruzky@gmail.com>
2016-08-24 15:43:07 +0700

    tambah relasi

--- app/Article.php
+++ app/Article.php
@@ -4,17 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Comment;
+
 class Article extends Model
 {
     protected $fillable = [
           'title', 'content', 'author', 'foto', 
-        ]; //
-         public static function valid($id='') {
-      	 return array(
+    ]; //
+    public static function valid($id='') {
+      return array(
         'title' => 'required|min:5|unique:articles,title'.($id ? ",$id" : ''),
         'content' => 'required|min:10|unique:articles,content'.($id ? ",$id" : ''),
         'author' => 'required',
         'foto' => 'mimes:jpeg,bmp,png|max:500'
-      );
+        );
     }
+
+    public function comments() {
+      return $this->hasMany('App\Comment', 'article_id');
+    }
+
 }
